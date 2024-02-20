64f3138761f6760635a87866cde2430686789be8
DanTheManGude <dgude31@gmail.com>
2024-02-19 21:25:40 -0500

    worksNames and WorksKey

--- src/constants.ts
+++ src/constants.ts
@@ -52,12 +52,30 @@ export const pagePathToKey: { [key in PagePath]: PageKey } =
 
 export type WorksPageKey = Exclude<PageKey, typeof CONTACT_KEY>;
 
-export const worksMap: { [key in WorksPageKey]: string[] } = {
-  [HOME_KEY]: [""],
-  DIGITAL: [""],
-  PACKAGING: [""],
-  PRINT: [""],
-  WEB: [""],
+export const worksNames = {
+  CALM_THINKING_AMAZON: "Calm Thinking Amazon",
+  ORGANIC_SOCIAL: "Variety of Organic Social Posts",
+  PROMOTIONAL_LANDING_PAGE: "Promotional Landing Page Design",
+  LITERATURE_AND_PACKAGING: "Variety of Literature and Packaging",
+  MULTIVITAMIN_PRODUCT: "Multivitamin Product Design",
+  TYPE_DREAMS_BRANDING: "Type Dreams Branding",
+} as const;
+
+export type WorksKey = keyof typeof worksNames;
+
+export const worksMap: { [key in WorksPageKey]: WorksKey[] } = {
+  [HOME_KEY]: [
+    "CALM_THINKING_AMAZON",
+    "ORGANIC_SOCIAL",
+    "PROMOTIONAL_LANDING_PAGE",
+    "LITERATURE_AND_PACKAGING",
+    "MULTIVITAMIN_PRODUCT",
+    "TYPE_DREAMS_BRANDING",
+  ],
+  DIGITAL: [],
+  PACKAGING: [],
+  PRINT: [],
+  WEB: [],
 };
 
 export const accentColor = "#e3e5f3";
