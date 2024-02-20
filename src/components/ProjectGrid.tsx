bdae6a4393b3a96a62b81ac91d07a9c8e31b65f6
DanTheManGude <dgude31@gmail.com>
2024-02-19 22:51:27 -0500

    overlay always on tile

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 import { WorksPageKey, WorksKey, worksMap, worksNames } from "@/constants";
 
@@ -13,6 +14,7 @@ function ProjectTile({ worksKey }: { worksKey: WorksKey }) {
       sx={{
         width: "100%",
         maxWidth: "400px",
+        position: "relative",
       }}
     >
       <Image
@@ -23,10 +25,29 @@ function ProjectTile({ worksKey }: { worksKey: WorksKey }) {
         sizes="100vw"
         style={{ width: "100%", height: "auto" }}
       />
+      <Box
+        sx={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          top: "0",
+          backgroundColor: "rgba(0,0,0,.5)",
+        }}
+      >
+        <Typography
+          variant="body2"
+          sx={{
+            position: "absolute",
+            width: "100%",
+            top: "50%",
+          }}
+          color="primary"
+        >
+          {worksName}
+        </Typography>
+      </Box>
     </Box>
   );
-
-  //<Typography variant="body2">{worksName}</Typography>;
 }
 
 export default function ProjectGrid({ pageKey }: { pageKey: WorksPageKey }) {
