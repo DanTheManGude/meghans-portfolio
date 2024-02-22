cbc7392c5f3996e3ded1f6cce71fe582706769f4
DanTheManGude <dgude31@gmail.com>
2024-02-22 01:20:16 -0500

    fix footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -23,57 +23,58 @@ export default function Footer() {
       sx={{ marginTop: "auto", width: "100vw", backgroundColor: accentColor }}
     >
       <Box sx={{ paddingLeft: "20px", display: { xs: "none", md: "flex" } }}>
-        <Stack>
-          <Stack direction="row">
-            {pageKeys.map((pageKey) => (
-              <Button
-                key={pageKey}
-                sx={{ marginTop: "20px", color: "white", display: "block" }}
-              >
-                <Link href={`/${pagePaths[pageKey]}`}>
-                  <Typography
-                    fontSize={"18px"}
-                    color="secondary"
-                    fontWeight={600}
-                    sx={{
-                      textDecoration:
-                        pathname === `/${pagePaths[pageKey]}`
-                          ? "underline"
-                          : "none",
-                    }}
-                  >
-                    {pageNames[pageKey]}
-                  </Typography>
-                </Link>
-              </Button>
-            ))}
-          </Stack>
-          <Stack direction="row" sx={{ paddingBottom: "10px" }}>
-            <Link href={"mailto:gudedesigns@gmail.com"}>
-              <IconButton size="large" color="secondary">
-                <EmailOutlinedIcon fontSize="large" />
-              </IconButton>
-            </Link>
-            <Link
-              href={"https://www.linkedin.com/in/meghangude/"}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <IconButton size="large" color="secondary">
-                <LinkedInIcon fontSize="large" />
-              </IconButton>
-            </Link>
-          </Stack>
-        </Stack>
-        <Box
-          sx={{
-            position: "absolute",
-            right: "20px",
-            bottom: "15px",
-          }}
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          sx={{ width: "100%" }}
         >
-          <Image src={logo} alt="logo" width={80} height={80} />
-        </Box>
+          <Stack>
+            <Stack direction="row">
+              {pageKeys.map((pageKey) => (
+                <Button
+                  key={pageKey}
+                  sx={{ marginTop: "20px", color: "white", display: "block" }}
+                >
+                  <Link href={`/${pagePaths[pageKey]}`}>
+                    <Typography
+                      fontSize={"18px"}
+                      color="secondary"
+                      fontWeight={600}
+                      sx={{
+                        textDecoration:
+                          pathname === `/${pagePaths[pageKey]}`
+                            ? "underline"
+                            : "none",
+                      }}
+                    >
+                      {pageNames[pageKey]}
+                    </Typography>
+                  </Link>
+                </Button>
+              ))}
+            </Stack>
+            <Stack direction="row" sx={{ paddingBottom: "10px" }}>
+              <Link href={"mailto:gudedesigns@gmail.com"}>
+                <IconButton size="large" color="secondary">
+                  <EmailOutlinedIcon fontSize="large" />
+                </IconButton>
+              </Link>
+              <Link
+                href={"https://www.linkedin.com/in/meghangude/"}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <IconButton size="large" color="secondary">
+                  <LinkedInIcon fontSize="large" />
+                </IconButton>
+              </Link>
+            </Stack>
+          </Stack>
+          <Box paddingRight={"20px"}>
+            <Image src={logo} alt="logo" width={80} height={80} />
+          </Box>
+        </Stack>
       </Box>
       <Stack
         spacing={1}
