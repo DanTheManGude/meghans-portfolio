d120e44d9e9770deedf67a9552ffb1352de5899f
DanTheManGude <dgude31@gmail.com>
2024-03-31 10:45:17 -0400

    cleanup unused components

--- src/components/SubTitle.tsx
+++
@@ -1,9 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-import { pageNames, PageKey, HOME_KEY } from "@/constants";
-
-export default function SubTitle({ pageKey }: { pageKey: PageKey }) {
-  if (pageKey === HOME_KEY) return null;
-
-  return <Typography variant="h4">{pageNames[pageKey]}</Typography>;
-}
