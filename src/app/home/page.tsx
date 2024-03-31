a7b3700bf04f590a05e7c4b402fda32fe0f97144
DanTheManGude <dgude31@gmail.com>
2024-03-31 12:03:43 -0400

    title and subtitle on main page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,67 @@
+import { PropsWithChildren } from "react";
+
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 
 import ProjectGrid from "@/components/ProjectGrid";
 
+const Title = (props: PropsWithChildren) => (
+  <Typography
+    sx={{
+      fontSize: { xs: 23, md: 45 },
+    }}
+    fontWeight={600}
+    textAlign="center"
+  >
+    {props.children}
+  </Typography>
+);
+
+const Subtitle = (props: PropsWithChildren) => (
+  <Typography
+    sx={{
+      fontSize: { xs: 14, md: 22 },
+      paddingTop: 1,
+    }}
+    textAlign="center"
+  >
+    {props.children}
+  </Typography>
+);
+
 export default function Page() {
   return (
-    <Container maxWidth="xl" sx={{ marginY: 4 }}>
-      Home
+    <Container
+      maxWidth="xl"
+      sx={{
+        marginY: 4,
+        paddingTop: 3,
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          paddingBottom: 10,
+          textAlign: "center",
+        }}
+        display="flex"
+        justifyContent="center"
+      >
+        <Stack
+          sx={{ width: { xs: "100%", md: 700, mx: "auto" } }}
+          alignItems="center"
+        >
+          <Title>
+            I'm Meghan Butera, designer and creative based in New York.
+          </Title>
+          <Subtitle>
+            Specializing in branding, packaging, and digital design.
+          </Subtitle>
+        </Stack>
+      </Box>
+
       <ProjectGrid />
     </Container>
   );
