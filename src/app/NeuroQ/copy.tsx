ccde98d646435c95b9d66af0b7a9de47b8a902aa
DanTheManGude <dgude31@gmail.com>
2024-07-03 17:53:00 -0400

    copy file

---
+++ src/app/NeuroQ/copy.tsx
@@ -0,0 +1,2 @@
+export const TITLE = "NeuroQ";
+export const DESCRIPTION = `When I was brought on as the Senior Graphic Designer for NeuroQ at LifeSeasons, they were In their beginning stages of branding, advertising and overall creative voice. Over the last two plus years I've worked to bring a new life to the brand to enhance sales and customer journey. Below are some examples.`;
