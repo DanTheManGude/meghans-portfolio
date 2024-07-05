c1afc785b52adf4aa58935aadec840da2c4dffe4
DanTheManGude <dgude31@gmail.com>
2024-07-05 16:41:55 -0400

    grid on main page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren } from "react";
 
+import Image from "next/image";
+import Link from "next/link";
+
+import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
+import { projectNames, projectOrder } from "@/constants";
 
 const titleText = "I'm Meghan Butera, designer and creative based in New York.";
 const subTitleText = "Specializing in branding, packaging, and digital design.";
@@ -56,6 +61,76 @@ export default function Page() {
         >
           <Title>{titleText}</Title>
           <Subtitle>{subTitleText}</Subtitle>
+          <Grid
+            container
+            spacing={5}
+            alignItems="center"
+            sx={{ paddingTop: "30px", paddingBottom: "15px" }}
+            textAlign="center"
+          >
+            {projectOrder.map((projectKey) => {
+              const overlayId = `${projectKey}-overlay`;
+
+              return (
+                <Grid
+                  key={projectKey}
+                  item
+                  xs={12}
+                  md={6}
+                  style={{ display: "flex", justifyContent: "center" }}
+                >
+                  <Link href={`/${projectNames[projectKey]}`}>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        maxWidth: "400px",
+                        position: "relative",
+                        [`& #${overlayId}`]: {
+                          opacity: 0,
+                        },
+                        [`& #${overlayId}:hover`]: {
+                          opacity: 1,
+                        },
+                        cursor: "pointer",
+                      }}
+                    >
+                      <Image
+                        priority={true}
+                        src={`/images/projectThumbnails/${projectKey}.jpg`}
+                        alt={`Project ${projectNames[projectKey]} thumbnail`}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: "100%", height: "auto" }}
+                      />
+                      <Box
+                        id={overlayId}
+                        sx={{
+                          position: "absolute",
+                          width: "100%",
+                          height: "100%",
+                          top: "0",
+                          backgroundColor: "rgba(0,0,0,.5)",
+                        }}
+                      >
+                        <Typography
+                          variant="body2"
+                          sx={{
+                            position: "absolute",
+                            width: "100%",
+                            top: "50%",
+                          }}
+                          color="primary"
+                        >
+                          {projectNames[projectKey]}
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Link>
+                </Grid>
+              );
+            })}
+          </Grid>
         </Stack>
       </Box>
     </Container>
