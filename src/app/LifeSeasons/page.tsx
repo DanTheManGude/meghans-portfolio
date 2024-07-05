039b89cc9a2a2eaa182c5647b1fcd3eb5cb44ebb
DanTheManGude <dgude31@gmail.com>
2024-07-05 16:16:40 -0400

    new project pages

---
+++ src/app/LifeSeasons/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+
+import { TileInfo, projectKeys, projectSections } from "@/constants";
+import ProjectTitle from "@/components/ProjectTitle";
+import ProjectDescription from "@/components/ProjectDescription";
+import ProjectSection from "@/components/ProjectSection";
+import ProjectDialog from "@/components/ProjectDialog";
+import NextProjectButton from "@/components/NextProjectButton";
+
+const projectKey = projectKeys.SUPPLEMENT;
+
+export default function Page() {
+  const [openingTargetTile, setOpeningTargetTile] = useState<TileInfo>({
+    sectionKey: projectSections[projectKey][0].key,
+    index: 0,
+  });
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const closeDialog = () => setIsDialogOpen(false);
+
+  const openDialog = (targetTile: TileInfo) => {
+    setOpeningTargetTile(targetTile);
+    setIsDialogOpen(true);
+  };
+
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        marginY: 4,
+        paddingTop: 3,
+      }}
+    >
+      <Stack alignItems={"center"} width={"100%"}>
+        <ProjectTitle projectKey={projectKey} />
+        <ProjectDescription projectKey={projectKey} sx={{ paddingBottom: 2 }} />
+        {projectSections[projectKey].map((sectionInfo) => (
+          <ProjectSection
+            key={sectionInfo.key}
+            projectKey={projectKey}
+            sectionKey={sectionInfo.key}
+            openDialog={openDialog}
+          />
+        ))}
+        <NextProjectButton currentProjectKey={projectKey} />
+      </Stack>
+      <ProjectDialog
+        projectKey={projectKey}
+        isDialogOpen={isDialogOpen}
+        closeDialog={closeDialog}
+        openingTargetTile={openingTargetTile}
+      />
+    </Container>
+  );
+}
