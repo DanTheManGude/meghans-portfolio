1d665fb99ed0b7261c3a1735ea8c480895dfd004
DanTheManGude <dgude31@gmail.com>
2024-07-15 23:49:14 -0400

    remove unused import

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,6 +1,5 @@
 import { accentColor, VideoInfo } from "@/constants";
 import Video from "next-video";
-import { Asset } from "next-video/dist/assets.js";
 
 export default function VideoPlayer(props: { videoInfo: VideoInfo }) {
   const {
