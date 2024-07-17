bbec134084f1f9cd5dc768452dcb8c0edeacb603
DanTheManGude <dgude31@gmail.com>
2024-07-17 00:25:10 -0400

    add LS page

--- src/app/LifeSeasons/page.tsx
+++ src/app/LifeSeasons/page.tsx
@@ -1,54 +1,7 @@
-"use client";
+import { projectKeys } from "@/constants";
 
-import { useState } from "react";
-
-import Container from "@mui/material/Container";
-import Stack from "@mui/material/Stack";
-
-import { TileInfo, projectKeys, projectSections } from "@/constants";
-import ProjectTitle from "@/components/ProjectTitle";
-import ProjectSection from "@/components/ProjectSection";
-import ProjectDialog from "@/components/ProjectDialog";
-import NextProjectButton from "@/components/NextProjectButton";
-
-const projectKey = projectKeys.LIFE_SEASONS;
+import ProjectPage from "@/components/ProjectPage";
 
 export default function Page() {
-  const [openingTargetTile, setOpeningTargetTile] = useState<TileInfo>();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const closeDialog = () => setIsDialogOpen(false);
-
-  const openDialog = (targetTile: TileInfo) => {
-    setOpeningTargetTile(targetTile);
-    setIsDialogOpen(true);
-  };
-
-  return (
-    <Container
-      maxWidth="xl"
-      sx={{
-        marginY: 4,
-        paddingTop: 3,
-      }}
-    >
-      <Stack alignItems={"center"} width={"100%"}>
-        <ProjectTitle projectKey={projectKey} />
-        {projectSections[projectKey].map((sectionInfo) => (
-          <ProjectSection
-            key={sectionInfo.key}
-            projectKey={projectKey}
-            sectionKey={sectionInfo.key}
-            openDialog={openDialog}
-          />
-        ))}
-        <NextProjectButton currentProjectKey={projectKey} />
-      </Stack>
-      <ProjectDialog
-        projectKey={projectKey}
-        isDialogOpen={isDialogOpen}
-        closeDialog={closeDialog}
-        openingTargetTile={openingTargetTile}
-      />
-    </Container>
-  );
+  return <ProjectPage projectKey={projectKeys.LIFE_SEASONS} />;
 }
--- src/copy.tsx
+++ src/copy.tsx
@@ -1,5 +1,5 @@
 export const NEURO_Q_DESCRIPTION = `When I was brought on as the Senior Graphic Designer for NeuroQ at LifeSeasons, they were In their beginning stages of branding, advertising and overall creative voice. Over the last two plus years I've worked to bring a new life to the brand to enhance sales and customer journey. Below are some examples.`;
-export const LIFE_SEASONS_DESCRIPTION = `LifeSeasons description`;
+export const LIFE_SEASONS_DESCRIPTION = `I worked on the LifeSeasons team as the Senior Graphic Designer to support their retail and e-commerce efforts. Below are some examples.`;
 export const SUPPLEMENT_DESCRIPTION = `Supplement description`;
 export const EMAIL_DESCRIPTION = `Email description`;
 
@@ -11,6 +11,11 @@ export const NEURO_Q_SOCIAL_DESCRIPTION = `Instagram and Facebook posts reiterat
 export const NEURO_Q_LANDING_PAGES_DESCRIPTION = `Serving as a place for our digital ads to funnel to, landing pages were used in our digital marketing campaigns. Here are two examples of landing page designs created to be educational and ultimately convert to sales.`;
 export const NEURO_Q_VIDEO_DESCRIPTION = `Providing educational content to our consumers was a top priority. I created videos related to our blog content to be posted on our website as well as YouTube. These included a variety of topics from ingredients, conditions and overall product knowledge.`;
 
+export const LIFE_SEASONS_PRODUCT = `I worked on the packaging design for one of LifeSeasons brands, ReCODE. These supplements are neurologist developed, highly individualized formulas that target brain health. Two of the products Daily Essentials and Daily Balance can be taken individually, or combined into our Core Kit to be taken together.`;
+export const LIFE_SEASONS_VIDEO = `Boostng our Amazon content was a top priority. Working with the Amazon team I created short videos under 30 seconds to add to each of our product listings. These are made to be informative about the product and ingredients while also capturing the customers atenton.`;
+export const LIFE_SEASONS_AMAZON = `With Amazon holding such a strong online shopping presence and a preferred method for many people it was important to have the imagery match the brand. I created new carousel images and A+ content for all products.`;
+export const LIFE_SEASONS_RETAIL = `Retail assets were important to support the sales team by giving them printed literature, posters, end caps and more to become a high selling brand in health food stores across the country. These graphics were displayed and distributed at stores such as Sprouts, Whole Foods, The Vitamin Shoppe, Mother's Market, Natural Grocers and more.`;
+
 export const PROJECT_HEADER_BLOCK_SCOPE_TITLE = "Scope:";
 export const PROJECT_HEADER_BLOCK_SKILLS_USED_TITLE = "Skills Used:";
 export const PROJECT_HEADER_BLOCK_TOOLS_USED_TITLE = "Tools Used:";
@@ -21,3 +26,10 @@ export const PROJECT_HEADER_BLOCK_SKILLS_USED_BODY_NEURO_Q =
   "Branding • Layout • UX Design • Digital Design • Print and Packaging Design • Video Editing";
 export const PROJECT_HEADER_BLOCK_TOOLS_USED_BODY_NEURO_Q =
   "Photoshop • Illustrator • Figma • Sketch • Premiere Pro • Dimensions";
+
+export const PROJECT_HEADER_BLOCK_SCOPE_BODY_LIFE_SEASONS =
+  "Promote e-commerce and retail efforts, boost sales, and educate the customer";
+export const PROJECT_HEADER_BLOCK_SKILLS_USED_BODY_LIFE_SEASONS =
+  "Digital Design • Print and Packaging Design • Video Editing";
+export const PROJECT_HEADER_BLOCK_TOOLS_USED_BODY_LIFE_SEASONS =
+  "Photoshop • Illustrator • Premiere Pro";
