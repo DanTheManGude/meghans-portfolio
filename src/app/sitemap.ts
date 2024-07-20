b78b888d0039eb62efb5a332614f1ad57c2c61ec
DanTheManGude <dgude31@gmail.com>
2024-07-20 13:24:01 -0400

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,25 +1,42 @@
-import { ABOUT_KEY, HOME_KEY, pageKeys, pagePaths } from "@/constants";
+import {
+  ABOUT_KEY,
+  HOME_KEY,
+  pageKeys,
+  pagePaths,
+  projectOrder,
+  projectPaths,
+} from "@/constants";
 import { MetadataRoute } from "next";
 
 const BASE_URL = "https://dev.gudedesigns.com/";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return pageKeys.map((pageKey) => {
-    let priority = 0.5;
-    switch (pageKey) {
-      case HOME_KEY:
-        priority = 1;
-        break;
-      case ABOUT_KEY:
-        priority = 0.8;
-        break;
-      default:
-        break;
-    }
-    return {
-      url: `${BASE_URL}/${pagePaths[pageKey]}`,
-      lastModified: new Date(),
-      priority,
-    };
-  });
+  return pageKeys
+    .map((pageKey) => {
+      let priority = 0.4;
+      switch (pageKey) {
+        case HOME_KEY:
+          priority = 1;
+          break;
+        case ABOUT_KEY:
+          priority = 0.8;
+          break;
+        default:
+          break;
+      }
+      return {
+        url: `${BASE_URL}/${pagePaths[pageKey]}`,
+        lastModified: new Date(),
+        priority,
+      };
+    })
+    .concat(
+      projectOrder.map((projectKey) => {
+        return {
+          url: `${BASE_URL}/${projectPaths[projectKey]}`,
+          lastModified: new Date(),
+          priority: 0.6,
+        };
+      })
+    );
 }
