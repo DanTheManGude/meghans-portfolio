ce16a29971f327bec8d4933af01211624f4fab55
DanTheManGude <dgude31@gmail.com>
2024-07-19 21:44:32 -0400

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
+
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
 
 import { accentColor, emailUrl, linkedinUrl } from "@/constants";
 
@@ -12,43 +15,75 @@ const paragraphTwo = `Shoot me an email to discuss your project, let's create so
 export default function Page() {
   return (
     <Container maxWidth="xl" sx={{ marginY: 4 }}>
-      <Box sx={{ width: { xs: "100%", md: "75%" } }}>
-        <Divider
-          sx={{
-            marginTop: "15px",
-            marginBottom: "15px",
-            borderBottomWidth: 8,
-            borderColor: accentColor,
-          }}
-        />
-        <Typography variant="body2">{paragraphOne}</Typography>
-        <br />
-        <Typography variant="body2">{paragraphTwo}</Typography>
-        <br />
-        <Typography fontWeight={600}>{`Contact:`}</Typography>
-        <Link
-          href={emailUrl}
-          sx={{
-            textDecoration: "none",
-          }}
+      <Grid
+        width={"100%"}
+        container
+        rowSpacing={5}
+        justifyContent="space-around"
+      >
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{ display: "flex", justifyContent: "center" }}
         >
-          <Typography
-            variant="body2"
-            color={"#5967af"}
-          >{`Gudedesigns@gmail.com`}</Typography>
-        </Link>
-        <Link
-          href={linkedinUrl}
-          sx={{
-            textDecoration: "none",
-          }}
+          <Box sx={{ width: "100%" }}>
+            <Divider
+              sx={{
+                marginTop: "15px",
+                marginBottom: "15px",
+                borderBottomWidth: 8,
+                borderColor: accentColor,
+              }}
+            />
+            <Typography variant="body2">{paragraphOne}</Typography>
+            <br />
+            <Typography variant="body2">{paragraphTwo}</Typography>
+            <br />
+            <Typography fontWeight={600}>{`Contact:`}</Typography>
+            <Link
+              href={emailUrl}
+              sx={{
+                textDecoration: "none",
+              }}
+            >
+              <Typography
+                variant="body2"
+                color={"#5967af"}
+              >{`Gudedesigns@gmail.com`}</Typography>
+            </Link>
+            <Link
+              href={linkedinUrl}
+              sx={{
+                textDecoration: "none",
+              }}
+            >
+              <Typography
+                variant="body2"
+                color={"#5967af"}
+              >{`Linkedin`}</Typography>
+            </Link>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={4}
+          style={{ display: "flex", justifyContent: "center" }}
         >
-          <Typography
-            variant="body2"
-            color={"#5967af"}
-          >{`Linkedin`}</Typography>
-        </Link>
-      </Box>
+          <Box sx={{ width: "100%" }}>
+            <Image
+              priority={true}
+              src={`/images/portfolio-pic.jpg`}
+              alt={`Photo of myself`}
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+            />
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
